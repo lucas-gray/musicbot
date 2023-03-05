090770b4f9c1f803e3c35c966521f85997d0d44f
Lucas Gray <lucasginak427@gmail.com>
2023-03-04 22:50:32 -0700

    Create basic bot that can respond to messages

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import discord
 
-def readtoken():
-    pass
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("/play"):
+        await message.channel.send("Hell no, that shit is trash.")
+    if message.content.startswith("fuck you"):
+        await message.channel.send("fuck you!")
+
+def read_token(filename):
+    file = open(filename, "r")
+    token = file.read().strip()
+    file.close()
+    return token
 
 def main():
-    pass
+    filename = "token.txt"
+    client.run(read_token(filename))
 
 if __name__ == "__main__":
     main()
