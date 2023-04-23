e4922f8efa356759e6e60717b8fe6a472b729482
Lucas Gray <lucasginak427@gmail.com>
2023-04-23 17:19:53 -0600

    Fix bugs, bot can join and leave

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import discord
 from discord.ext import commands
+import asyncio
 import music
 
+client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
 def read_token(filename):
     file = open(filename, "r")
@@ -18,15 +15,9 @@ def read_token(filename):
     file.close()
     return token
 
-def main():
-    cogs = [music]
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Bot(command_prefix='!', intents=intents)
-    for i in range(len(cogs)):
-        cogs[i].setup(client)
-    filename = "token.txt"
-    client.run(read_token(filename))
+async def main():
+    await music.setup(client)
+    async with client:
+        await client.start(read_token("token.txt"))
 
-if __name__ == "__main__":
-    main()
+asyncio.run(main())
--- music.py
+++ music.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+import yt_dlp
 
-class music(commands.Cog):
+class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -26,10 +26,10 @@ class music(commands.Cog):
         FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", "options": "-vn"}
         YDL_OPTIONS = {"format": "bestaudio"}
         vc = ctx.voice_client
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info["formats"][0]["url"]
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            source = await discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS)
             vc.play(source)
 
     @commands.command()
@@ -40,6 +40,6 @@ class music(commands.Cog):
     async def resume(self, ctx):
         await ctx.voice_client.resume()
 
-def setup(client):
-    client.add_cog(music(client))
+async def setup(client):
+    await client.add_cog(Music(client))
 
